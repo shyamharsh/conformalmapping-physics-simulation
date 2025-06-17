6492e547647082b160a4925f690f3189b33543f7
shyamharsh <t3wika00@students.oamk.fi>
2025-06-18 01:03:21 +0300

    configure multi page build for vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: 'index.html',
+        standing: 'standing.html',
+        decay: 'decay.html',
+        complex_z: 'complex_z.html',
+        complex_z2: 'complex_z2.html',
+        complex_z3: 'complex_z3.html'
       },
     },
   },
